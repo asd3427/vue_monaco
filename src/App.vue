64bc9180bfb8ab49bfd8dd195d7793cd2c04923e
abulaga <abulaga@users.noreply.github.com>
2020-05-22 16:21:02 +0800

    feat : add button

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,15 @@
 				</select>
 			</div>
 			<h2>Monaco Editor Auto-completion test for <code id='code'>{{getTypeSelected()}}</code> files</h2>
+			<button v-on:click="wrap()">點我</button>
 		</div>
 	</div>
 </template>
 <script>
 	// import MonacoEditor from "vue-monaco";
 	import * as monaco from "monaco-editor";
+	
+	import editor from  "monaco-editor";
 	
 	export default {
 		name: "app",
@@ -32,41 +35,41 @@
 					];
 				}
 			});
-			monaco.languages.registerSignatureHelpProvider(language, {
-				signatureHelpTriggerCharacters: ["(", ","],
-				// eslint-disable-next-line no-unused-vars
-				provideSignatureHelp: (model, position, token) => {
-					return {
-						activeParameter: 0,
-						activeSignature: 0,
-						signatures: [
-							{
-								label:
-									"string substr(string $string, int $start [, int $length])",
-								parameters: [
-									{
-										label: "string $string",
-										documentation:
-											"The input string. Must be one character or longer."
-									},
-									{
-										label: "int $start",
-										documentation:
-											"If $start is non-negative, the returned string will start at the $start'th position in string, counting from zero. For instance, in the string 'abcdef', the character at position 0 is 'a', the character at position 2 is 'c', and so forth.\r\nIf $start is negative, the returned string will start at the $start'th character from the end of string. If $string is less than $start characters long, FALSE will be returned."
-									},
-									{
-										label: "int $length",
-										documentation:
-											"If $length is given and is positive, the string returned will contain at most $length characters beginning from $start (depending on the length of $string) If $length is given and is negative, then that many characters will be omitted from the end of $string (after the start position has been calculated when a start is negative). If $start denotes the position of this truncation or beyond, FALSE will be returned. If $length is given and is 0, FALSE or NULL, an empty string will be returned. If $length is omitted, the substring starting from $start until the end of the string will be returned."
-									}
-								]
-							}
-						]
-					};
-				}
-			});
+			// monaco.languages.registerSignatureHelpProvider(language, {
+			// 	signatureHelpTriggerCharacters: ["(", ","],
+			// 	// eslint-disable-next-line no-unused-vars
+			// // 	provideSignatureHelp: (model, position, token) => {
+			// // 		return {
+			// // 			activeParameter: 0,
+			// // 			activeSignature: 0,
+			// // 			signatures: [
+			// // 				{
+			// // 					label:
+			// // 						"string substr(string $string, int $start [, int $length])",
+			// // 					parameters: [
+			// // 						{
+			// // 							label: "string $string",
+			// // 							documentation:
+			// // 								"The input string. Must be one character or longer."
+			// // 						},
+			// // 						{
+			// // 							label: "int $start",
+			// // 							documentation:
+			// // 								"If $start is non-negative, the returned string will start at the $start'th position in string, counting from zero. For instance, in the string 'abcdef', the character at position 0 is 'a', the character at position 2 is 'c', and so forth.\r\nIf $start is negative, the returned string will start at the $start'th character from the end of string. If $string is less than $start characters long, FALSE will be returned."
+			// // 						},
+			// // 						{
+			// // 							label: "int $length",
+			// // 							documentation:
+			// // 								"If $length is given and is positive, the string returned will contain at most $length characters beginning from $start (depending on the length of $string) If $length is given and is negative, then that many characters will be omitted from the end of $string (after the start position has been calculated when a start is negative). If $start denotes the position of this truncation or beyond, FALSE will be returned. If $length is given and is 0, FALSE or NULL, an empty string will be returned. If $length is omitted, the substring starting from $start until the end of the string will be returned."
+			// // 						}
+			// // 					]
+			// // 				}
+			// // 			]
+			// // 		};
+			// // 	}
+			//  });
 			
-			const mo = monaco.editor.create(document.getElementById("container"), {
+			var mo = monaco.editor.create(document.getElementById("container"), {
 				value: "",
 				language: language,
 				theme: "vs-dark",
@@ -76,13 +79,22 @@
 					enabled: false // 关闭小地图-->
 				},
 			});
-			 mo.onDidChangeModelContent((event) => {
-				// 编辑器内容change事件
-				 console.log(mo.getValue());
-				
-			 });
 			
+			mo.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function () { // 使用
+				var t= mo.getValue()
+				console.log('Ctrl + S 保存')
+				console.log(t)
+				
+			})
+			this.edits = mo
+			// mo.onDidChangeCursorPosition(e => {
+			// 	// console.log('Cursor changed', mo.getPosition());
+			//
+			//
+			// });
+			//
 		},
+		
 		
 		
 		// 選單
@@ -94,20 +106,26 @@
 					{id: 3, name: 'c++'},
 				],
 				selected: '',
-				
+				edits:null
 			}
-		},
+		}
+		,
 		created() {
 			//如果没有这句代码，select中初始化会是空白的，默认选中就无法实现
 			this.selected = this.typeList[0].name;
 			
-		},
+		}
+		,
 		methods: {
-			
 			getTypeSelected() {
 				return this.selected
+			},
+			wrap(){
+				console.log(this.edits.getValue())
+				
 			}
-		},
+		}
+		,
 		
 		
 	};
